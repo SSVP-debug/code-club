5c34977f928621b75c8959c1d98b9cc17ff5c710
Shiva Shankara Vara Prasad <ssvp006@gmail.com>
2026-09-25 12:20:53 +0530

    chore: remove JSX comment ambiguity in learning workspace

--- src/components/learning/LearningWorkspace.jsx
+++ src/components/learning/LearningWorkspace.jsx
@@ -58,7 +58,6 @@ function LearningWorkspace({
         </div>
       </div>
 
-      {/* Cards */}
       <ContinueLearningCard />
       <DailyMissionCard submissions={submissions} currentStreak={currentStreak} />
       <AICoachCard
